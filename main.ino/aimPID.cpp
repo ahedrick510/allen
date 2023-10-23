fd7f11a2f7598f995b69c26f77a0d842bb3a2067
ahedrick510 <alexander.hedrick@colorado.edu>
2023-10-23 17:49:57 -0600

    Create aimPID.cpp

---
+++ main.ino/aimPID.cpp
@@ -0,0 +1,15 @@
+// #include <stdint.h> // maybe need to include?
+
+void PixyControl(uint16_t x){
+    // variables
+    uint16_t r = 158;
+    double p = 1;
+    double i = 0;
+    double d = 0;
+
+    // calculate error
+    double error = x - r;
+    
+
+
+}
