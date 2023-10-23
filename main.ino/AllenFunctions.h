c3b82ecffaf6a13e0c449c6ae0ef291b1e82bcfa
ahedrick510 <alexander.hedrick@colorado.edu>
2023-10-23 16:40:26 -0600

    added main and header file

---
+++ main.ino/AllenFunctions.h
@@ -0,0 +1,7 @@
+#ifndef ALLENFUNCTIONS_H
+#define ALLENFUNCTIONS_H
+
+// Put function declarations here
+void blinkLED(int ledPin, int delayTime); //this is just an example
+
+#endif
---
+++ main.ino/MotorControl.cpp
@@ -0,0 +1,5 @@
+#include <Pixy2.h>
+
+void PixyControl(){
+  
+}
