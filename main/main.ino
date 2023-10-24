dd417cfc87f30047b328972bc9ab2b60509365ac
JuneNita <junenita@gmail.com>
2023-10-24 02:07:29 -0600

    add accidentally deleted file

--- main/main.ino
+++ main/main.ino
@@ -1,1 +1,12 @@
+#include <Pixy2.h>
+#include "AllenFunctions.h"
 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
