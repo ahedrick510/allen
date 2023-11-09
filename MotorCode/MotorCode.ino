9f974fe4e16416c3b59e9f2a34507821a82750a5
ahedrick510 <alexander.hedrick@colorado.edu>
2023-11-08 18:58:38 -0700

    Update MotorCode.ino
    code when robot worked very well on 11/8. added p controller and cleaned up the code a bit

--- MotorCode/MotorCode.ino
+++ MotorCode/MotorCode.ino
@@ -26,10 +26,13 @@ void setupPixy();
 Block getPixyBlockData();
 uint16_t getPixyXCoord();
 uint16_t getPixyColor();
-uint16_t x;
-unsigned int pause = 50;
+uint16_t x; // x location of enemy robot
+unsigned int pause = 25;
 bool isTurningLeft = false;
 bool isTurningRight = false;
+const int p = 1; // p value for motor speed
+int turning_speed = 0; // motor turning speed when it sees enemy robot
+const int middle = 158; // middle of pixy view
 
 //long duration, cm, dist;
 
@@ -66,68 +69,95 @@ bool isFR_S_Active = false;
 bool isFL_S_Active = false;
 
 void loop() {
-  isBR_S_Active = digitalRead(BR_S) == 0;
-  isBL_S_Active = digitalRead(BL_S) == 0;
-  isFR_S_Active = digitalRead(FR_S) == 0;
-  isFL_S_Active = digitalRead(FL_S) == 0;
+//  isBR_S_Active = digitalRead(BR_S) == 0;
+//  isBL_S_Active = digitalRead(BL_S) == 0;
+//  isFR_S_Active = digitalRead(FR_S) == 0;
+//  isFL_S_Active = digitalRead(FL_S) == 0;
 
-  // CHANGE THESE VALUES LATER
-  Block pixyBlocks = getPixyBlockData();
+//  Serial.println(isBR_S_Active);
+//  Serial.println(isBL_S_Active);
+//  Serial.println(isFR_S_Active);
+//  Serial.println(isFL_S_Active);
+
+  ///////// OLD GET X ///////////
+//  Block pixyBlocks = getPixyBlockData();
   x = getPixyXCoord();
-  bool targetIsOnTheLeft = x < 150; // 0-315
-  bool targetIsOnTheRight = x > 165;
-  bool shoot = x > 150 && x < 165;
+  Serial.print("x = ");
+  Serial.println(x);
+  
+  bool targetIsOnTheLeft = x < (middle - 10); // 0-315
+  bool targetIsOnTheRight = x > (middle + 10);
+  bool shoot = x >= (middle - 10) && x <= (middle + 10);
 
-  Serial.println(isBR_S_Active);
-  Serial.println(isBL_S_Active);
-  Serial.println(isFR_S_Active);
-  Serial.println(isFL_S_Active);
-
-
+  Serial.println(targetIsOnTheLeft);
+  Serial.println(targetIsOnTheRight);
+  Serial.println(shoot);
   if (!isFL_S_Active && !isFR_S_Active && !isBL_S_Active && !isBR_S_Active) {
-    forward(); // Move forward when all sensors are inactive
-  } else if (isFR_S_Active || isFL_S_Active) {
-    backward(); // Move backward if any of the front sensors are active
-  } else if (isBR_S_Active) {
-    turnLeft(); // Turn left if back right sensor active
-  } else if (isBL_S_Active) {
-    turnRight(); // Turn right if back left sensor active
-  } else {
-    //pixy
+
+    ////////////////// ROOMBA STYLE MOVING /////////////////////
+    // HERE
+    ////////
+    
+    ////////////////// PIXY CODE ////////////////////////////
+
+    ///// MOTOR SPEED P CONTROL //////////
+    turning_speed = p*(middle - x);
+    turning_speed = map(abs(turning_speed), 0, 158, 10, 125); // changing turning speed to pwm control (puts value between 10 and 200 out of 255)
+    Serial.print("turning speed = ");
+    Serial.println(turning_speed);
+    
     if (targetIsOnTheLeft) {
       Serial.println("left");
       isTurningLeft = true;
       isTurningRight = false;
-      turnLeft();
+      turnLeft(turning_speed);
     }
     else if (targetIsOnTheRight) {
       Serial.println("right");
       isTurningLeft = false;
       isTurningRight = true;
-      turnRight();
+      turnRight(turning_speed);
     }
     else if (shoot) {
       Serial.println("shoot");
       if (isTurningLeft) {
-        turnRight();
+        turnRight(200);
         delay(pause);
         Serial.println("stopLeft");
         stopMotor();
       } else if (isTurningRight) {
-        turnLeft();
+        turnLeft(200);
         delay(pause);
         Serial.println("stopRight");
         stopMotor();
       } else {
         // Handle the case when the direction is not determined
       }
-      delay(5000);
+      delay(1000);
+
+    targetIsOnTheLeft = 0;
+    targetIsOnTheRight = 0;
+    shoot = 0;
+    
     }
     else {
       // Move forward if none of the above conditions are met
-      forward();
+      forward(100);
     }
+
+
+  /////////////////// IR SENSOR CODE ////////////////////////////////////////////  
+  } else if (isFR_S_Active || isFL_S_Active) {
+    backward(100); // Move backward if any of the front sensors are active
+  } else if (isBR_S_Active) {
+    turnLeft(100); // Turn left if back right sensor active
+  } else if (isBL_S_Active) {
+    turnRight(100); // Turn right if back left sensor active
+  } else {
+    
   }
+
+  ////////////////// OLD IR SENSOR CODE /////////////////////////
 
   //if (isFR_S_Active) {
   //    backward(); // Move forward when all sensors are inactive
@@ -155,6 +185,7 @@ void loop() {
 //  delay(250);
 //}
 
+//////////////////// MOTOR FUNCTIONS ////////////////////////
 void stopMotor() {
   digitalWrite(AIN1, LOW); //Motor A Rotate Counter Clockwise
   digitalWrite(AIN2, LOW);
@@ -165,40 +196,40 @@ void stopMotor() {
 }
 
 /* need to callibrate the direction and PWM value */
-void forward() {
+void forward(int w) {
   digitalWrite(AIN1, LOW); //Motor A Rotate Counter Clockwise
   digitalWrite(AIN2, HIGH);
   digitalWrite(BIN1, LOW); //Motor B Rotate Counter Clockwise
   digitalWrite(BIN2, HIGH);
-  analogWrite(PWM1, 200);
-  analogWrite(PWM2, 200);
+  analogWrite(PWM1, w);
+  analogWrite(PWM2, w);
 }
 
-void backward() {
+void backward(int w) {
   digitalWrite(AIN1, HIGH);
   digitalWrite(AIN2, LOW);
   digitalWrite(BIN1, HIGH);
   digitalWrite(BIN2, LOW);
-  analogWrite(PWM1, 100);
-  analogWrite(PWM2, 100);
+  analogWrite(PWM1, w);
+  analogWrite(PWM2, w);
 }
 
-void turnRight() {
+void turnRight(int w) {
   digitalWrite(AIN1, HIGH); // 1 = left motor, 2 = right motor, both HIGH = forward, both LOW = backward
   digitalWrite(AIN2, LOW);
   digitalWrite(BIN1, HIGH);
   digitalWrite(BIN2, LOW);
-  analogWrite(PWM1, 100);
-  analogWrite(PWM2, 100);
+  analogWrite(PWM1, w);
+  analogWrite(PWM2, w);
 }
 
-void turnLeft() {
+void turnLeft(int w) {
   digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, HIGH);
   digitalWrite(BIN1, LOW);
   digitalWrite(BIN2, HIGH);
-  analogWrite(PWM1, 100);
-  analogWrite(PWM2, 100);
+  analogWrite(PWM1, w);
+  analogWrite(PWM2, w);
 }
 
 void setupPixy() {
@@ -210,13 +241,20 @@ Block getPixyBlockData() {
   if (pixy.ccc.numBlocks) {
     //Serial.print("Detected ");
     //Serial.println(pixy.ccc.numBlocks);
-    pixy.ccc.blocks[0].print();
+//    pixy.ccc.blocks[0].print();
     return pixy.ccc.blocks[0];
+    
   }
 }
 
 uint16_t getPixyXCoord() {
-  return getPixyBlockData().m_x;
+  pixy.ccc.getBlocks();
+  if (pixy.ccc.numBlocks) {
+    //Serial.print("Detected ");
+    //Serial.println(pixy.ccc.numBlocks);
+//    pixy.ccc.blocks[0].print();
+    return pixy.ccc.blocks[0].m_x;
+  }
 }
 
 uint16_t getPixyColor() {
